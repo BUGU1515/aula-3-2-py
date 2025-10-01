3f3c630d56e99c58ccf439d598a41da3e56fe89e
BUGU1515 <juliotrindade@colegiopaulodetarso.com.br>
2025-10-01 15:43:09 +0000

    .

---
+++ ex2.py
@@ -0,0 +1,8 @@
+def aula(matéria):
+    if matéria == "matemática":
+        print("Eu estudei matemática ontem!")
+    else:
+        print(f"Eu estudei {matéria} ontem!")
+
+aula("matemática")
+# aula("história")
